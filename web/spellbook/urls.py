a053f720bedd3c1f57d0cc0af65bb843c9a95df1
Trich <trich@inevitableleftturns.com>
2021-06-30 15:03:03 -0500

    added proper expansion and sort views

--- web/spellbook/urls.py
+++ web/spellbook/urls.py
@@ -16,6 +16,6 @@ Including another URLconf
 from django.urls import re_path, include
 
 urlpatterns = [
-    re_path(r'^$', include('apps.home.urls')),
+    re_path(r'^', include('apps.home.urls')),
     re_path(r'samples', include('apps.home.urls')),
 ]
